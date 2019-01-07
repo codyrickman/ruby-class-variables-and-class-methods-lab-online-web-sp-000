9b5e808122844fe24d66f74329886d8278875b08
Cody Rickman <cody@creativemindstudios.com>
2019-01-07 01:38:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
 class Song
   attr_accessor :name, :artist, :genre
+  @@count = 0
   def initialize (name, artist, genre)
     self.name = name
     self.artist = artist
     self.genre = genre
-    @@count = 0
+    @@count += 1
   end
   def count
     return @@count
